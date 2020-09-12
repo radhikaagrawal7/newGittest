46a08c451cfa9e66234ec889647e83d4a17695f2
Radhika Agrawal <radhikaagrawal@Radhikas-MacBook-Pro.local>
2020-09-12 18:11:53 -0400

    added index.fs

---
+++ Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+open Akka.FSharp
+open Akka.Actor
+//[<EntryPoint>]
+//let main argv =
+    //printfn "Hello World from F#!"
+    //0 // return an integer exit code
+let system = System.create "system" (Configuration.defaultConfig())
+type greeter = 
+    | Hello of string
+    | Goodbye of string
+let greeter = spawn system "greeter" <| fun mailbox ->
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with
+        | Hello name -> printf "Hello, %s!\n" name
+        | Goodbye name -> printf "Goodbye, %s!\n" name
+        return! loop()
+    }
+    loop()
+greeter <! Hello "Joe"
+greeter <! Goodbye "Joe"
+System.Console.ReadLine() |> ignore
